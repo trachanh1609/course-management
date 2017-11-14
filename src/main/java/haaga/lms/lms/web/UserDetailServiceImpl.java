ccf94134ceea9e5e032fe8fba137745bc77debb7
trachanh1609 <Vu@Users-MBP.home>
2017-11-14 16:12:57 +0200

    add security config

---
+++ src/main/java/haaga/lms/lms/web/UserDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package haaga.lms.lms.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import haaga.lms.lms.domain.User;
+import haaga.lms.lms.domain.UserRepository;
+
+/**
+ * This class is used by spring security to authenticate and authorize user
+ **/
+@Service
+public class UserDetailServiceImpl implements UserDetailsService  {
+	private final UserRepository repository;
+
+	@Autowired
+	public UserDetailServiceImpl(UserRepository userRepository) {
+		this.repository = userRepository;
+	}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {   
+    	User curruser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
+        		AuthorityUtils.createAuthorityList(curruser.getRole()));
+        return user;
+    }   
+} 
